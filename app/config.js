146d6850d5834df14d001155ef99577973099a26
philswan <mail@philswan.co.uk>
2016-06-15 15:59:14 +0100

    Getting some initial questions in

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Design patterns",
+  serviceName: "Visiting Service Management System",
 
   // Default port that prototype runs on
   port: '3000',
--- app/routes.js
+++ app/routes.js
@@ -5,4 +5,14 @@ router.get('/', function (req, res) {
   res.render('index');
 });
 
+// Step 1
+router.post('/step1-referrer', function (req, res) {
+  res.redirect('/step2-customer-details');
+});
+
+// Step 2
+router.post('/step2-customer-details', function (req, res) {
+  res.redirect('/step3-customer-contact');
+});
+
 module.exports = router;
