88bef056fd87871aadd58c5e0600552570388dea
philswan <mail@philswan.co.uk>
2016-06-16 16:13:13 +0100

    More content

--- app/routes.js
+++ app/routes.js
@@ -5,14 +5,52 @@ router.get('/', function (req, res) {
   res.render('index');
 });
 
-// Step 1
-router.post('/step1-referrer', function (req, res) {
-  res.redirect('/step2-customer-details');
+// Referrer
+router.post('/1-referrer', function (req, res) {
+  res.redirect('/2-customer-details');
 });
 
-// Step 2
-router.post('/step2-customer-details', function (req, res) {
-  res.redirect('/step3-customer-contact');
+// Customer
+router.post('/2-customer-details', function (req, res) {
+  res.redirect('/2-customer-contact');
+});
+
+router.post('/2-customer-contact', function (req, res) {
+  res.redirect('/3-representative-details');
+});
+
+// Representative
+router.post('/3-representative-details', function (req, res) {
+  res.redirect('/3-representative-contact');
+});
+
+router.post('/3-representative-contact', function (req, res) {
+  res.redirect('/4-visit-details');
+});
+
+// Visit
+router.post('/4-visit-details', function (req, res) {
+  res.redirect('/4-visit-address');
+});
+
+router.post('/4-visit-address', function (req, res) {
+  res.redirect('/4-visit-other');
+});
+
+router.post('/4-visit-other', function (req, res) {
+  res.redirect('/5-attachments');
+});
+
+// Attachments
+
+router.post('/5-attachments', function (req, res) {
+  res.redirect('/6-check-your-answers');
+});
+
+// Check your answers
+
+router.post('/6-check-your-answers', function (req, res) {
+  res.redirect('/7-confirmation');
 });
 
 module.exports = router;
