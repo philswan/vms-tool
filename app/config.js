7465f76ba5a3b07024fb54ba9c3132e83bd904b1
philswan <mail@philswan.co.uk>
2016-08-22 12:30:35 +0100

    Update to service name and first stab at postcode lookup

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Visiting Service Management System",
+  serviceName: "Request a visit",
 
   // Default port that prototype runs on
   port: '3000',
