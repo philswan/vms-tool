9d7870521e4faf89ff31ec4ccebf012ef5b9493e
philswan <mail@philswan.co.uk>
2016-09-20 14:53:11 +0100

    Adds v3

--- app/routes.js
+++ app/routes.js
@@ -256,5 +256,129 @@ router.get('/v2/check-your-answers/saved', function (req, res) {
   res.render('v2/check-your-answers', {'saved':true} );
 });
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+// V3 ROUTES
+////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// REFERRER
+/////////////////////////
+
+
+// Referrer
+router.post('/v3/referrer', function (req, res) {
+  res.redirect('/v3/customer-nino');
+});
+
+// Referrer details check
+router.get('/v3/referrer/edit', function (req, res) {
+  res.render('v3/referrer', {'edit':true} );
+});
+
+// CUSTOMER
+/////////////////////////
+
+// Customer National Insurance number
+router.post('/v3/customer-nino', function (req, res) {
+  res.redirect('/v3/customer-details');
+});
+
+// Customer National Insurance check
+router.get('/v3/customer-nino/edit', function (req, res) {
+  res.render('v3/customer-nino', {'edit':true} );
+});
+
+
+// Customer details
+router.post('/v3/customer-details', function (req, res) {
+  res.redirect('/v3/customer-contact');
+});
+
+// Customer details check
+router.get('/v3/customer-details/edit', function (req, res) {
+  res.render('v3/customer-details', {'edit':true} );
+});
+
+// Customer contact
+router.post('/v3/customer-contact', function (req, res) {
+  res.redirect('/v3/representative-details');
+});
+
+// Customer contact check
+router.get('/v3/customer-contact/edit', function (req, res) {
+  res.render('v3/customer-contact', {'edit':true} );
+});
+
+// REPRESENTATIVE
+/////////////////////////
+
+// Representative details
+router.post('/v3/representative-details', function (req, res) {
+  res.redirect('/v3/visit-details');
+});
+
+// Representative details check
+router.get('/v3/representative-details/edit', function (req, res) {
+  res.render('v3/representative-details', {'edit':true} );
+});
+
+
+// VISIT
+/////////////////////////
+
+
+// Visit details
+router.post('/v3/visit-details', function (req, res) {
+  res.redirect('/v3/visit-address');
+});
+
+// Visit details check
+router.get('/v3/visit-details/edit', function (req, res) {
+  res.render('v3/visit-details', {'edit':true} );
+});
+
+// Visit address
+router.post('/v3/visit-address', function (req, res) {
+  res.redirect('/v3/visit-other');
+});
+
+// Visit address check
+router.get('/v3/visit-address/edit', function (req, res) {
+  res.render('v3/visit-address', {'edit':true} );
+});
+
+// Visit other
+router.post('/v3/visit-other', function (req, res) {
+  res.redirect('/v3/attachments');
+});
+
+// Visit other check
+router.get('/v3/visit-other/edit', function (req, res) {
+  res.render('v3/visit-other', {'edit':true} );
+});
+
+// ATTACHMENTS
+/////////////////////////
+
+// Attachments
+router.post('/v3/attachments', function (req, res) {
+  res.redirect('/v3/check-your-answers');
+});
+
+// Attachments check
+router.get('/v3/attachments/edit', function (req, res) {
+  res.render('v3/attachments', {'edit':true} );
+});
+
+// Check your answers
+router.post('/v3/check-your-answers', function (req, res) {
+  res.redirect('/v3/confirmation');
+});
+
+// Check your answers save
+router.get('/v3/check-your-answers/saved', function (req, res) {
+  res.render('v3/check-your-answers', {'saved':true} );
+});
 
 module.exports = router;
